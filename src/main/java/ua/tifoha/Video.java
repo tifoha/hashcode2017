1848f6f59b2933b4055813217a45967134e79c4f
tifoha <tifoha@gmail.com>
2017-02-23 21:44:56 +0200

    dsaSF

---
+++ src/main/java/ua/tifoha/Video.java
@@ -0,0 +1,31 @@
+package ua.tifoha;
+
+/**
+ * Created by Vitaliy Sereda on 23.02.17.
+ */
+public class Video {
+	public final int id;
+	public final int size;
+
+	public Video(int id, int size) {
+		this.id = id;
+		this.size = size;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder(this.getClass().getSimpleName());
+		sb.append("V[").append(id);
+		sb.append(": ").append(size);
+		sb.append(']');
+		return sb.toString();
+	}
+}
