d72e1182ad8ed447ee808c154b082ad0ea0dd8d5
Vasyl Zarva <vasyan.z@gmail.com>
2017-02-23 22:13:59 +0200

    calculator improvement

--- src/main/java/ua/google/team/hashcode/commons/ScoreCalculator.java
+++ src/main/java/ua/google/team/hashcode/commons/ScoreCalculator.java
@@ -7,9 +7,14 @@ public final class ScoreCalculator {
     }
 
     public static BigDecimal of(Long globalLatency, Long localLatency, Long requestsCount, Long fileSize) {
-        return BigDecimal.valueOf(globalLatency)
-                .subtract(BigDecimal.valueOf(localLatency))
-                .multiply(BigDecimal.valueOf(requestsCount))
+
+        BigDecimal glVsLocal = BigDecimal.valueOf(globalLatency)
+                  .subtract(BigDecimal.valueOf(localLatency));
+
+        if (glVsLocal.longValue() <= 0) // todo compare with 0? maybe with percent of global latency
+            return new BigDecimal(0);
+
+        return glVsLocal.multiply(BigDecimal.valueOf(requestsCount))
                 .divide(BigDecimal.valueOf(fileSize), BigDecimal.ROUND_UNNECESSARY);
     }
 }
